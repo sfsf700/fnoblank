81d7b9686e5fe1bdc92c94f847811d3a0897c7e3
sfsf700 <hirokazu.2rnk@gmail.com>
2022-08-07 11:59:43 +0900

    編集ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'fishings#index'
-  resources :fishings, only: [:index, :new, :create, :show]
+  resources :fishings, only: [:index, :new, :create, :show, :edit, :update]
 
 end
