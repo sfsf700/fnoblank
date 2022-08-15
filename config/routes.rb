ec669f73576c45f6e0f5dafeb3c90af97cd0c682
sfsf700 <hirokazu.2rnk@gmail.com>
2022-08-15 16:13:53 +0900

    マイページ情報編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'fishings#index'
+  resources :users, only: [:edit, :update]
   resources :fishings do
     resources :comments, only: :create
     resource :favorites, only: [:create, :destroy]
